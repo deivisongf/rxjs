cae3c6673b40d7cc86b0a9085b02252c85c7c7f4
Deivison <dguarines@gmail.com>
2020-01-20 13:01:58 -0300

    async com ngif

--- src/app/async/async.component.ts
+++ src/app/async/async.component.ts
@@ -2,6 +2,11 @@ import { Component, OnInit } from '@angular/core';
 import { Observable } from 'rxjs';
 import { map, toArray, delay } from 'rxjs/operators';
 
+interface User {
+  login: string;
+  name: string;
+}
+
 @Component({
   selector: 'app-async',
   templateUrl: './async.component.html',
@@ -10,6 +15,7 @@ import { map, toArray, delay } from 'rxjs/operators';
 export class AsyncComponent implements OnInit {
 
   private options$:Observable<string[]>;
+  private user$:Observable<User>;
 
   constructor() { }
 
@@ -28,7 +34,24 @@ export class AsyncComponent implements OnInit {
       delay(2000)
     );
 
-    this.options$.subscribe(s => console.log(s));
+    //this.options$.subscribe(s => console.log(s));
+
+    this.user$ = new Observable<User>((observer) => {
+      let names = ["Deivison", "Rafael", "Allan", "Tyago"];
+      let logins = ["deivison.furtado", "rafael.duarte", "allan.radji", "tyago.safadao"];
+      let i = 0;
+      setInterval(() => {
+        if(i == 4)
+          observer.complete()
+        else {
+          observer.next({login: logins[i], name: names[i]});
+        }
+        i++;
+
+      }, 3000)
+    })
+
+    //this.user$.subscribe(s => console.log(s));
   }
 
 }
